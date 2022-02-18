b4c52dcb652783ee68051a8a20d21d181c6fa803
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2022-02-19 00:09:56 +0530

    added sort colors

---
+++ Day-1-Array-Part1/05_Sort_Colors(optimal).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// bruteforce can be done by counting 0,1,2 and replace original array
+
+// optimal O(N) small variation of dutch national flag algortihm
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
